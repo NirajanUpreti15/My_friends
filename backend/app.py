c6a30313e22a452e7bc06c896b82c053014e575a
Nirajan Upreti <nirajanupreti15@gmail.com>
2026-09-15 11:31:16 +0545

    Fix backend submodule and upload actual backend files

---
+++ backend/app.py
@@ -0,0 +1,45 @@
+#UPDATE THS FILE FOR DEPLOYMENT
+
+from flask import Flask,send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+
+app = Flask(__name__)
+CORS(app)
+#cors package is to wrap the entire app so we can get request from client and send respond to clinet
+# when we send request from client we use json.stringfy 
+#while swndingto clint data we jsonify the data
+
+
+#we create data with this config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+frontend_folder=os.path.join(os.getcwd(),"..","frontend")
+dist_folder=os.path.join(frontend_folder,"dist")
+
+#server static files from the "dist"folder under the "frontend" directory
+
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+   if not filename:
+      filename="index.html"
+   return send_from_directory(dist_folder,filename)
+
+
+#api routes
+
+
+
+
+import routes
+#to create table
+with app.app_context():
+ db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
